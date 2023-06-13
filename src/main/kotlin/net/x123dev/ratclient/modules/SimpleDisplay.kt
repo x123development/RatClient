3cd4c2df5f7e0be0ca0b6f8265699f0fac11f279
x123dev <mr.x123123123@gmail.com>
2023-06-13 13:10:51 +0200

    some extra images

--- src/main/kotlin/net/x123dev/ratclient/modules/SimpleDisplay.kt
+++ src/main/kotlin/net/x123dev/ratclient/modules/SimpleDisplay.kt
@@ -30,9 +30,12 @@ fun browseWebsite(url:String){
 fun fullScreenImage(title:String){
 
     fullScreenImageFromURL(when(title){
+        "stupid"->"https://image.spreadshirtmedia.com/image-server/v1/mp/products/T1459A839PA3861PT28D1036131088W8333H10000/views/1,width=1200,height=630,appearanceId=839,backgroundColor=F2F2F2/if-youre-reading-this-youre-stupid-haha-black-sticker.jpg"
+        "gaykiss"->"https://s3.amazonaws.com/lavendermagazine-site/wp-content/uploads/2018/07/bigstock-Gay-Couple-Kissing-in-Paulista-222170278.jpg"
+        "hacked"->"https://pghnetworks.com/wp-content/uploads/2018/06/Blog-pic.jpg"
         "crash"->"https://www.itmagazine.ch/imgserver/artikel/Illustrationen/2020/mid/Bsodwindows10.png_200824_100851.jpg"
         "leobad"->"https://i.imgur.com/3FgicVg.png"
-        "gay"->"https://pbs.twimg.com/media/B4vhLhBIgAAFjj0.jpg"
+        "gaysex"->"https://pbs.twimg.com/media/B4vhLhBIgAAFjj0.jpg"
         "clown"-> "https://st2.depositphotos.com/1194063/6307/i/950/depositphotos_63079503-stock-photo-clown.jpg"
         "kekw"->"https://uploads-ssl.webflow.com/604d8aa059681eb15035fd3e/63644ab1a48040d9f35be665_KEKW%20Emote%20Origin%20and%20history.png"
         else->"https://uploads-ssl.webflow.com/604d8aa059681eb15035fd3e/63644ab1a48040d9f35be665_KEKW%20Emote%20Origin%20and%20history.png"
